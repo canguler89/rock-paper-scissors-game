ac7711c3cd8abd9b0dd8bebdf9226b5f221e0b9c
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-02-10 12:39:29 -0600

    app is done and  ready to play

--- app.js
+++ app.js
@@ -6,14 +6,51 @@ const rock = document.getElementById("rock");
 const restart = document.getElementById("blank");
 const gameScore = document.getElementById("gameScore");
 const options = document.querySelectorAll("#options");
+const playerPick = document.getElementById("playerPick");
+const computerPick = document.getElementById("computerPick");
 
 function refreshPage() {
   location.reload();
 }
 restart.addEventListener("click", refreshPage);
 
-options.addEventListener("click", play);
+options.forEach(choice => choice.addEventListener("click", play));
 
 function play(e) {
-  console.log(e.target.value);
+  const playerChoice = e.target.id;
+  console.log(playerChoice);
+
+  const number = Math.floor(Math.random() * 3);
+  if (number === 0) {
+    computerChoice = "rock";
+  } else if (number === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  console.log(computerChoice);
+
+  if (computerChoice === playerChoice) {
+    gameScore.innerHTML = "it is a tie";
+  } else if (computerChoice === "rock") {
+    if (playerChoice === "paper") {
+      gameScore.innerHTML = "Player wins";
+    } else {
+      gameScore.innerHTML = "Computer wins";
+    }
+  } else if (computerChoice === "paper") {
+    if (playerChoice === "scissors") {
+      gameScore.innerHTML = "Player wins";
+    } else {
+      gameScore.innerHTML = "Computer wins";
+    }
+  } else if (computerChoice === "scissors") {
+    if (playerChoice === "rock") {
+      gameScore.innerHTML = "Player wins";
+    } else {
+      gameScore.innerHTML = "Computer wins";
+    }
+  }
+  computerPick.innerHTML = computerChoice;
+  playerPick.innerHTML = playerChoice;
 }
